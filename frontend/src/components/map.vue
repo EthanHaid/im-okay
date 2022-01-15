cdb6ef30814e503fac1e781f0bea97c9649298b2
Ethan Haid <ethanhaid@gmail.com>
2022-01-15 14:48:37 -0800

    harp without npm

--- frontend/src/components/map.vue
+++ frontend/src/components/map.vue
@@ -1,18 +1,42 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue';
-import { Harp } from "@/services/harp";
+// import { Harp } from "@/services/myharp";
+// import * as THREE from "@/services/three.js"
+// import * as harp from "@/services/harp.js"
 
-const mapCanvas = ref();
+const canvasRef = ref();
 
 onMounted(() => {
-  Harp.initializeMapView(mapCanvas.value)
+  const canvas = canvasRef.value;
+  const mapView = new harp.MapView({
+    canvas,
+    theme: "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json"
+  });
+
+  // TODO: base coords on backend data
+  mapView.lookAt({
+    target: new harp.GeoCoordinates(40.70398928, -74.01319808),
+    zoomLevel: 17,
+    tilt: 40,
+  });
+
+  const mapControls = new harp.MapControls(mapView);
+  const ui = new harp.MapControlsUI(mapControls);
+  canvas.parentElement.appendChild(ui.domElement);
+
+  mapView.resize(window.innerWidth, window.innerHeight);
+  window.onresize = () => mapView.resize(window.innerWidth, window.innerHeight);
+
+  const vectorTileDataSource = new harp.VectorTileDataSource({
+    authenticationCode: "61cG0CqLLHlSzg5-OFXCt22CbT3jR6GU4f4kl-JS-1A"
+  });
+  mapView.addDataSource(vectorTileDataSource);
 })
 
 </script>
 
 <template>
-  <h1>Disaster Recovery Map</h1>
-  <canvas ref="mapCanvas" />
+  <canvas ref="canvasRef" />
 </template>
 
 <style scoped>

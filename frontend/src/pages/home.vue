0c31db9f4dfb3795240d09e4e42cabd1593d92ff
Jennifer Dai <jydai@sfu.ca>
2022-01-16 00:17:16 -0800

    style home page

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -8,32 +8,45 @@ import VVanta from 'vue-vanta';
 const options = {
             mouseControls: true,
             touchControls: true,
-            minHeight: 500.00,
+            minHeight: 1000.00,
             minWidth: 200.00,
             scale: 1.00,
-            scaleMobile: 1.00
+            scaleMobile: 1.00,
+            color: 0x144470,
+            waveSpeed: 1.20,
         }
-
 </script>
 
 <template>
-  <v-vanta effect="globe" :options=options></v-vanta>
-  <div class="p-grid" id="container">
-  	<div class="p-col-6 sidebar">
-      <Title />
-    </div>
-  	<div class="p-col-6">
-      <Button class="btn" @click="$router.push('map')" label="View Map" />
-      <br />
-      <Button class="btn" @click="$router.push('send')" label="Send safety check" />
-    </div>
+  <div class="absolute">
+    <Title/>
+    <p>A visual application for getting people the help they need.</p>
+    <Button class="btn" @click="$router.push('map')" label="View Map" />&nbsp
+    <Button class="btn" @click="$router.push('send')" label="Send safety check" />
   </div>
+  <v-vanta id="bg" effect="waves" :options=options></v-vanta>
 </template>
 
 <style scoped>
-.sidebar {
+.absolute {
+    position: absolute;
+    left: 15vw;
+    top: 30vh;
+    font-size: 2em;
+    width: 40vw;
     background-color: var(--secondary);
-    width: 50vw;
-    height: 100vh;
+    padding: 1em;
+}
+.absolute > p {
+  font-size: 0.7em;
+  color: var(--primary-light);
+}
+
+#bg {
+  color: #999999;
+  width: 100vw;
+  height: 120vh;
+  position: fixed;
+  z-index: -100;
 }
 </style>

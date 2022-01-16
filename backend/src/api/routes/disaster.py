741659700e99708f50ee1fdd2d15945fd845f928
Asim Shrestha <ashresth@sfu.ca>
2022-01-15 22:19:29 -0800

    feat: add route to update

--- backend/src/api/routes/disaster.py
+++ backend/src/api/routes/disaster.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from ..helpers.FirebaseClient import FirebaseClient
-from ..models import DisasterInput, DisasterCreate, DisasterFireBase
+from ..models import DisasterInput, DisasterCreate, DisasterFireBase, DisasterResponseCreate
 from ..models import DisasterResponseInput, DisasterResponseFireBase
 from typing import List
 import datetime
@@ -36,13 +36,30 @@ def create_disaster(disaster_input: DisasterInput) -> DisasterFireBase:
     return disaster
 
 
+@router.put("/response/{disaster_id}/{disaster_response_id}")
+def update_disaster_response(disaster_id: str, disaster_response_id: str, disaster_response_input: DisasterResponseInput) -> DisasterFireBase:
+    firebase_client = FirebaseClient()
+    db = firebase_client.get_db()
+    disaster_response_record = db.child(PREFIX).child(disaster_id).child('responses').child(disaster_response_id)
+
+    # Update fields
+    disaster = disaster_response_record.update({
+        'is_ok': disaster_response_input.is_ok,
+        'message': disaster_response_input.message,
+        'location:': disaster_response_input.location.dict()
+    })
+
+    return disaster
+
+
 @router.post("/response/{disaster_id}")
 def create_disaster_response(disaster_id: str, disaster_response_input: DisasterResponseInput) -> DisasterFireBase:
     return handle_create_disaster_response(disaster_id, disaster_response_input)
 
 
 def handle_create_disaster_response(disaster_id: str, disaster_response_input: DisasterResponseInput):
-    disaster_response = DisasterResponseInput(**disaster_response_input.dict(), timestamp=datetime.datetime.now())
+    # Add timestamp
+    disaster_response = DisasterResponseCreate(**disaster_response_input.dict(), timestamp=datetime.datetime.now().__str__())
 
     firebase_client = FirebaseClient()
     db = firebase_client.get_db()

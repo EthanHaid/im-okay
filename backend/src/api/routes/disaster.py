b92beb17a289025cf9423a6513f905a6daec3afa
Asim Shrestha <ashresth@sfu.ca>
2022-01-15 17:54:07 -0800

    feat: basic routes

--- backend/src/api/routes/disaster.py
+++ backend/src/api/routes/disaster.py
@@ -1,17 +1,39 @@
 from fastapi import APIRouter
+
+from ..helpers.FirebaseClient import FirebaseClient
 from ..models import DisasterInput, DisasterFireBase
 from ..models import DisasterResponseInput, DisasterResponseFireBase
-router = APIRouter(prefix="/disaster")
+from typing import List
+
+PREFIX = 'disasters'
+router = APIRouter(prefix='/' + PREFIX)
+
+
+@router.get("/")
+def get_disasters() -> List[DisasterResponseFireBase]:
+    firebase_client = FirebaseClient()
+    db = firebase_client.get_db()
+    data = db.child(PREFIX).get()
+    return list(data)
 
 
 @router.get("/{disaster_id}")
 def get_disaster(disaster_id: str) -> DisasterResponseFireBase:
-    return 200
+    firebase_client = FirebaseClient()
+    db = firebase_client.get_db()
+    disaster = db.child(PREFIX).child(disaster_id).get()
+    return disaster
+
 
 @router.post("/")
 def create_disaster(disaster: DisasterInput) -> DisasterFireBase:
-    return 200
+    firebase_client = FirebaseClient()
+    db = firebase_client.get_db()
+    disaster = db.child(PREFIX).push(disaster.dict())
+    return disaster
+
 
 @router.post("/response")
 def create_disaster_response(disaster_response: DisasterResponseInput) -> DisasterResponseFireBase:
+
     return 200

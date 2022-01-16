c17ee048fca7840f183de139d40b858bd973a065
Adam Watkins <cadamrun@gmail.com>
2022-01-16 09:59:26 -0800

    Default Responses Now working

--- frontend/src/pages/status.vue
+++ frontend/src/pages/status.vue
@@ -14,7 +14,7 @@ import { DisasterAPI } from "@/api/disaster";
 const route = useRoute()
 var value = ""
 let location = {}
-let disasterApi = new DisasterAPI(String(route.query.disaster_id), String(route.query.disaster_response_id))
+let disasterApi = new DisasterAPI(route.query.disaster_id, route.query.disaster_response_id)
 setLocation()
 
 async function help() {

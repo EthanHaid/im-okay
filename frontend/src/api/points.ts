c9c04b370d6f3c0a12c1ff5673c8e97147b1c95a
Adam Watkins <cadamrun@gmail.com>
2022-01-16 01:12:50 -0800

    Points on map!

---
+++ frontend/src/api/points.ts
@@ -0,0 +1,37 @@
+import { db } from "@/firebase";
+
+class PointsApi {
+    disasterId: string
+
+    constructor(disasterId: string) {
+        this.disasterId = disasterId
+    }
+
+    async getPoints() {
+        const snapshot = await db.ref(`disasters/${this.disasterId}`).once('value')
+        const info = snapshot.val()
+
+        const features = []
+
+        console.log(info)
+
+        for (const [key, value] of Object.entries(info.responses)) {
+            // @ts-ignore
+            if (value.location?.lon != null && value.location?.lat != null) {
+                 features.push({
+                        type: "Feature",
+                        properties: {},
+                        geometry: {
+                            type: "Point",
+                            // @ts-ignore
+                            coordinates: [value.location.lon, value.location.lat]
+                        }
+                    })
+            }
+        }
+
+        return { type: "FeatureCollection", features }
+    }
+}
+
+export { PointsApi }
--- frontend/src/components/map.vue
+++ frontend/src/components/map.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue';
 import { db } from "@/firebase";
+import { PointsApi } from "@/api/points";
 
 declare const harp: any;
 
@@ -10,7 +11,21 @@ onMounted(() => {
   const canvas = canvasRef.value;
   const mapView = new harp.MapView({
     canvas,
-    theme: "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json",
+    theme: {
+      extends:
+          'https://unpkg.com/@here/harp-map-theme/resources/berlin_tilezen_effects_streets.json',
+      styles: {
+        geojson: [
+          {
+            when: ['==', ['geometry-type'], 'Point'],
+            technique: 'circles',
+            renderOrder: 10000,
+            color: '#ff6458',
+            size: 20,
+          }
+        ]
+      }
+    },
     projection: harp.sphereProjection
   });
 
@@ -26,14 +41,17 @@ onMounted(() => {
   });
   mapView.addDataSource(vectorTileDataSource);
 
-  db.ref('test').get().then(d => {
-    let { lat, lon } = d.val();
-    mapView.lookAt({
-      target: new harp.GeoCoordinates(lat, lon),
-      zoomLevel: 4,
-      tilt: 40,
-    });
-  })
+
+
+  new PointsApi("-MtW3zaUorRXshWPTJN3").getPoints().then(features => {
+      const dataProvider = new harp.GeoJsonDataProvider('features', features);
+      mapView.addDataSource(new harp.OmvDataSource({
+        name: 'static',
+        styleSetName: 'geojson',
+        dataProvider: dataProvider,
+        addGroundPlane: false,
+      })).then(() => { mapView.update()});
+  });
 })
 
 </script>

6756cb2626a1452ad184188ebcd6cc1a5bcc8f0c
Adam Watkins <cadamrun@gmail.com>
2022-01-16 08:40:22 -0800

    Add meta to points

--- frontend/src/api/points.ts
+++ frontend/src/api/points.ts
@@ -18,15 +18,26 @@ class PointsApi {
         for (const [key, value] of Object.entries(info.responses)) {
             // @ts-ignore
             if (value.location?.lon != null && value.location?.lat != null) {
-                 features.push({
+
+                features.push({
                         type: "Feature",
-                        properties: {},
+                        properties: {
+                                  // @ts-ignore
+                            isOk: value.is_ok,
+                                  // @ts-ignore
+                            message: value.message,
+                                  // @ts-ignore
+                            phoneNumber: value.message,
+                                  // @ts-ignore
+                            timestamp: value.timestamp
+                        },
                         geometry: {
                             type: "Point",
                             // @ts-ignore
                             coordinates: [value.location.lon, value.location.lat]
                         }
                     })
+
             }
         }
 

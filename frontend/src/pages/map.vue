39a76aced720887a272f716bea9d1ecd87d0ad55
Jennifer Dai <jydai@sfu.ca>
2022-01-15 17:37:52 -0800

    add filter sidebar

--- frontend/src/pages/map.vue
+++ frontend/src/pages/map.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
-import Map from '@/components/map.vue'
+import { ref } from 'vue';
+import Map from "@/components/map.vue"
 
 import Button from "primevue/button";
+import Checkbox from 'primevue/checkbox';
+import Sidebar from "primevue/sidebar";
+
+const visibleRight = ref(false);
 </script>
 
-<template>
-  <Button @click="$router.push('/')" label="Home" />
-  <Map />
+  <template>
+    <Button @click="$router.push('/')" label="Home" />
+    <Map />
+
+    <Sidebar v-model:visible="visibleRight" position="right">
+      <p>Filter by</p>
+    </Sidebar>
+
+    <Button
+      id="filter-button"
+      icon="pi pi-filter-fill"
+      class="p-button-rounded"
+      @click="visibleRight= true" />
+
 </template>
 
 <style scoped>
+#filter-button {
+  position: fixed;
+  bottom: 2em;
+  right: 2em;
+  background-color: var(--primary);
+  border: none;
+}
 </style>
--- frontend/src/pages/portal.vue
+++ frontend/src/pages/portal.vue
@@ -16,28 +16,4 @@ import Button from "primevue/button";
 </template>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Cabin:wght@600&family=Raleway:wght@300;400;600&display=swap");
-h2 {
-  background-color: red
-}
-:root {
-  --primary: #ff6458;
-  --primary-light: #ffd1ba;
-  --secondary: #355497;
-  --secondary-light: #6987c9;
-  --light: #fdf6ed;
-  --dark: #1a150f;
-  --display-font: "Cabin", sans-serif;
-  --text-font: "Raleway", sans-serif;
-}
-
-#app {
-  font-family: var(--text-font);
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  background-color: #ffffff;
-  color: var(--dark);
-  margin-top: 60px;
-}
 </style>

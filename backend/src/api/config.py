8bdd7e5ec552c66e29eae28835754dd9eecfe780
Adam Watkins <cadamrun@gmail.com>
2022-01-15 23:50:54 -0800

    Map into globe

--- backend/src/api/config.py
+++ backend/src/api/config.py
@@ -5,7 +5,7 @@ class _BaseConfig:
     TWILIO_ACCOUNT_SID: str = "ACfa91146369a012b0e912cd3719cb04d8"
     TWILIO_AUTH_TOKEN: str = "bbf8c32367b2783e998dc080fe5926f7"
     TWILIO_PHONE_NUMBER: str = "+19377644868"
-    FRONTEND_URL: str = "https://disaster-recovery.vercel.app/"
+    FRONTEND_URL: str = "https://imokay.tech/"
 
     def __init__(self):
         self._load_environment_variables()
